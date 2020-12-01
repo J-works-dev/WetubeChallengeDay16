3bd8228a40c6a598182f6c1e905fe6811f0e2a6e
SangJoon Lee(Jeremy) <sangjoon.jeremy.lee@gmail.com>
2020-12-01 09:58:05 +0800

    Update movieRouter.js
    final change

--- src/movieRouter.js
+++ src/movieRouter.js
@@ -9,8 +9,10 @@ import {
 const movieRouter = express.Router();
 
 movieRouter.get("/", home);
+
 movieRouter.get("/add", getAddMovie);
 movieRouter.post("/add", postAddMovie);
+
 movieRouter.get("/:id", movieDetail);
 
 export default movieRouter;
